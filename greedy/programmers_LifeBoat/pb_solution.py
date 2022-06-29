c8fbd9f861b1665e34458a7cd22a75fcf8a943ae
Dohui Park <doheepa@kakao.com>
2022-06-29 10:42:19 +0900

    refactor: remove people_in_boat

--- greedy/programmers_LifeBoat/pb_solution.py
+++ greedy/programmers_LifeBoat/pb_solution.py
@@ -1,21 +1,17 @@
 def solution(people, limit):
     people = sorted(people)
-    people_in_boat = [False] * len(people)
     answer = 0
     left, right = 0, len(people) - 1
     while left < right:
         boat = people[left] + people[right]
         if boat > limit:
-            people_in_boat[right] = True
             answer += 1
             right -= 1
         else:
-            people_in_boat[left] = True
-            people_in_boat[right] = True
             answer += 1
             left += 1
             right -= 1
-    return answer + people_in_boat.count(False)
+    return answer + 1 if left == right else answer
 
 
 print(solution([70, 50, 80, 50], 100) == 3)
